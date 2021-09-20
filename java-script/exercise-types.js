5459eb780ad780b45875589da830aca04ca025fd
GigaHierz <lenahierzi@hotmail.de>
2021-09-20 16:26:33 +0200

    Add js exercises

---
+++ java-script/exercise-types.js
@@ -0,0 +1,41 @@
+// write a function that adds to numbers together and returns the sum
+
+function sum(a, b) {
+
+};
+
+// sum(3 + 6)
+
+// write a function that adds to strings together and returns one string
+function concat(a, b) {
+
+};
+
+// concat('Lena', 'Hierzi')
+
+// write a function that returns the 1st element out of an Array
+function firstElement(array) {
+
+}
+
+// firstElement([1, 2, 3, 4])
+// firstElement(['apple', 'peach', 'pommegranat'])
+
+/*  write a function that returns an object including the following information
+   name: string,
+   githubName: string,
+   id: number,     ->(can be any rondom number)
+
+
+   EXTRA:
+   for the name use the concat function to add your first name + last name,
+   for the id use the sum function to add your favorite number to your age,
+   for that the inputs for name and id would be arrays. So you would need 
+   to check for the type. 
+*/
+function userInfo(name, githubName, id) {
+
+}
+
+// userInfo('Lena', 'giaghierz', 23)
+// userInfo(['Lena', 'Hierzi'], 'giaghierz', [23, 29])
