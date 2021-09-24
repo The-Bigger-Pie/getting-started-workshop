2b229cb0955be50a01e29d520ca013f60d184041
GigaHierz <lenahierzi@hotmail.de>
2021-09-24 19:00:52 +0200

    Add lenas solutions

--- java-script/exercise-types.js
+++ java-script/exercise-types.js
@@ -12,39 +12,72 @@
    -> number
 */
 
+/* To run the file, in the terminal, you have to enter this command:
+
+   `node name-of-file.js`
+
+
+   - make sure you are in the right directory in your terminal. You might have to 
+      add the directory the file is in... 
+      e.g. when your terminal shows you that you are at `getting-started-workshop` 
+      you have to run this command:
+
+      `node java-script/your-name.js`
+
+      or you navigate into the directory with 
+
+      `cd java-script`
+
+      and here you can run this command
+
+      `node your-name.js`
+   
+
+*/
 
 // write a function that adds to numbers together and returns the sum
 
 function sum(a, b) {
-
+   // console.log(typeof a);
+   // console.log(typeof b);
+   return a + b;
 };
 
-// sum(3 + 6)
+// var addSum = sum(3, 6);
+// console.log(addSum);
 
 // write a function that adds to strings together and returns one string
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/concat
 function concat(a, b) {
 
 };
 
-// concat('Lena', 'Hierzi')
+// var myName = concat('Lena', 'Hierzi');
+// console.log(myName);
 
 // write a function that checks if two inputs are equal
 // check out different levels of equality (e.g. strict equality)
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Equality_comparisons_and_sameness
 function isEqual() {
 
 }
 
-// isEqual(2, 3)
-// isEqual(2, 2)
-// isEqual(2, '2')
+// var first = isEqual(2, 3); // -> false
+// var second = isEqual(2, 2); // -> true
+// var third = isEqual(2, '2'); // -> true or false
+
+// console.log(first);
+// console.log(second);
+// console.log(third);
 
 // write a function that returns the 1st element out of an Array
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
 function firstElement(array) {
 
 }
 
-// firstElement([1, 2, 3, 4])
-// firstElement(['apple', 'peach', 'pommegranat'])
+//  console.log(firstElement([1, 2, 3, 4]));
+//  console.log(firstElement(['apple', 'peach', 'pommegranat']));
 
 /*  write a function that returns an object including the following information
    name: string,
@@ -62,7 +95,7 @@ function userInfo(name, githubName, id) {
 
 }
 
-// userInfo('Lena', 'giaghierz', 23)
-// userInfo(['Lena', 'Hierzi'], 'giaghierz', [23, 29])
+//  console.log(userInfo('Lena', 'giaghierz', 23))
+//  console.log(userInfo(['Lena', 'Hierzi'], 'giaghierz', [23, 29]))
 
 
